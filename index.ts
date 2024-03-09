7de8bea6f3b69496d2e8ee84c751fcd6ad333506
HansFigo <hambasahaya0303@gmail.com>
2024-03-10 00:42:29 +0700

    Elysia Setup

--- index.ts
+++ index.ts
@@ -1,4 +1,16 @@
-import { Koor } from "./src/data";
+import { Koor, repoInfo } from "./src/data";
+import logDivisiInfo from "./src/utils/logDivisiInfo";
 import logProfil from "./src/utils/logProfile";
+import { Elysia } from "elysia";
 
+logDivisiInfo();
+console.log("");
 logProfil(Koor);
+
+new Elysia()
+  .get("/", () => repoInfo)
+  .get("/koor", () => Koor)
+  .onError(({ code }) => {
+    if (code === "NOT_FOUND") return "LU MAU KMN BANGGG :(";
+  })
+  .listen(3000);
