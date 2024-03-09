887e1776cb8284c5a44e31aae0a9377fb398f049
HansFigo <hambasahaya0303@gmail.com>
2024-03-09 23:45:08 +0700

    Added Test Case for showing profile

---
+++ test/profile.test.ts
@@ -0,0 +1,46 @@
+import chalk from "chalk";
+import logProfile from "../src/utils/logProfile";
+
+console.log = jest.fn();
+
+test("logProfile correctly logs profile data", () => {
+  const profileData = {
+    name: "Figo",
+    divisi: "Web Frontend",
+    favLanguage: ["Typescript", "Javascript"],
+    waifu: ["Homura-chan"],
+  };
+
+  logProfile(profileData);
+
+  expect(console.log).toHaveBeenCalledTimes(5);
+
+  expect(console.log).toHaveBeenNthCalledWith(
+    1,
+    chalk.bold.rgb(255, 0, 255)("Profil Koor:"),
+  );
+  expect(console.log).toHaveBeenNthCalledWith(
+    2,
+    chalk.keyword("orange")("👨‍💻 Nama:") +
+      " " +
+      chalk.bold.green(profileData.name),
+  );
+  expect(console.log).toHaveBeenNthCalledWith(
+    3,
+    chalk.keyword("pink")("💼 Divisi:") +
+      " " +
+      chalk.italic.cyan(profileData.divisi),
+  );
+  expect(console.log).toHaveBeenNthCalledWith(
+    4,
+    chalk.keyword("blue")("💻 Bahasa Favorit:") +
+      " " +
+      chalk.underline.yellow(profileData.favLanguage.join(", ")),
+  );
+  expect(console.log).toHaveBeenNthCalledWith(
+    5,
+    chalk.keyword("purple")("💖 Waifu:") +
+      " " +
+      chalk.bgMagentaBright.whiteBright(profileData.waifu.join(", ")),
+  );
+});
