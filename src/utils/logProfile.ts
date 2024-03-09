accf229909581406c93e9376d05086da18d6da57
HansFigo <hambasahaya0303@gmail.com>
2024-03-09 23:29:12 +0700

    Added function to show profile to console

---
+++ src/utils/logProfile.ts
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import { Pengurus } from "../interface/perngurus";
+
+export const logProfil = (profileData: Pengurus) => {
+  console.log(chalk.bold.rgb(255, 0, 255)("Profil Koor:"));
+  console.log(
+    chalk.keyword("orange")("👨‍💻 Nama:") +
+      " " +
+      chalk.bold.green(profileData.name),
+  );
+  console.log(
+    chalk.keyword("pink")("💼 Divisi:") +
+      " " +
+      chalk.italic.cyan(profileData.divisi),
+  );
+  console.log(
+    chalk.keyword("blue")("💻 Bahasa Favorit:") +
+      " " +
+      chalk.underline.yellow(profileData.favLanguage?.join(", ")),
+  );
+  console.log(
+    chalk.keyword("purple")("💖 Waifu:") +
+      " " +
+      chalk.bgMagentaBright.whiteBright(profileData.waifu?.join(", ")),
+  );
+};
