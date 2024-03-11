03f51a7fb34cb71662587c1a50c3e3fc01884213
HansFigo <hambasahaya0303@gmail.com>
2024-03-12 01:32:52 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import { html } from "@elysiajs/html";
-import { tailwind } from "@gtramontina.com/elysia-tailwind";
 import { Elysia } from "elysia";
 import { autoroutes } from "elysia-autoroutes";
 import { logDivisiInfo, logServerStatus } from "./lib/utils/logDivisiInfo";
@@ -10,18 +9,6 @@ export const app = new Elysia()
     autoroutes({
       routesDir: "./routes", // -> optional, defaults to './routes'
       generateTags: false, // -> optional, defaults to true
-    }),
-  )
-  .use(
-    tailwind({
-      path: "/public/output.css",
-      source: "./src/app.css",
-      config: "./tailwind.config.ts",
-      options: {
-        minify: true,
-        map: true,
-        autoprefixer: false,
-      },
     }),
   )
   .listen(3000, ({ port, hostname }) => {
